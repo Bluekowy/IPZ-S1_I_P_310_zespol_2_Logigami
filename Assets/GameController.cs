d5c59f33c4fa1481260e333d355ad17a85106952
Jakub Lewan <Bluekowy@gmail.com>
2020-12-01 15:08:36 +0100

    Wypakowanie

---
+++ Assets/GameController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Photon.Pun;
+public class GameController : MonoBehaviourPun
+{
+    public Transform[] Spawn = null;
+    private void Awake()
+    {
+        int i = PhotonNetwork.CurrentRoom.PlayerCount - 1;
+        PhotonNetwork.Instantiate("PlayerA", Spawn[i].position, Spawn[i].rotation);
+    }
+}
