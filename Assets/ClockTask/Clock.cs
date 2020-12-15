b4d8d11984716b64b31e3dc9f016c5995856c625
Jakub Lewan <Bluekowy@gmail.com>
2020-12-15 11:22:52 +0100

    Próby zrobienia taska + interakcji z obiektami

---
+++ Assets/ClockTask/Clock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Clock : MonoBehaviour
+{
+    public int hour = 0;
+    public int minute = 0;
+    public TextMeshProUGUI time;
+
+    
+    public void changeTime(int x)
+    {
+        Debug.Log("kliknieto");
+        minute = minute + x;
+        if (minute >= 60)
+        {
+            minute = minute - 60;
+            hour = hour + 1;
+        }
+        time.SetText(hour + ";" + minute);
+    }
+}
---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    void OnTriggerEnter2d(Collider2D other)
+    {
+        if (other.tag=="interObject")
+        {
+            Debug.Log("dotknieto " + other.name);
+        }
+    }
+}
