8c043dd96fa82e08a3a806e79aa0d04598875bab
Jakub Lewan <Bluekowy@gmail.com>
2021-01-19 15:33:23 +0100

    Poprawki zegara i początek coop taska

--- Projekt/Assets/ClockTask/ClockTask.cs
+++ Projekt/Assets/ClockTask/ClockTask.cs
@@ -2,17 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ClockTask: MonoBehaviour
 {
     public int hour = 0;
     public int minute = 0;
     public TextMeshProUGUI time;
+    public GameObject clock1;
+    public GameObject clock2;
+    public GameObject clock3;
+    public GameObject clock4;
+    public GameObject clock5;
+    public GameObject clock6;
+    int ktoryClock;
+    int win;
+    public void onStart()
+    {
+        if (ktoryClock == 1)
+        {
+            clock1.SetActive(true);
+        }
+        else if (ktoryClock == 2)
+        {
+            clock2.SetActive(true);
+        }
+        else if (ktoryClock == 3)
+        {
+            clock3.SetActive(true);
+        }
+        else if (ktoryClock == 4) 
+        {
+            clock4.SetActive(true);
+        }
+        else if (ktoryClock == 5)
+        {
+            clock5.SetActive(true);
+        }
+        else if (ktoryClock == 6)
+        {
+            clock6.SetActive(true);
+        }
+    }
 
-    
     public void changeTime(int x)
     {
-        Debug.Log("kliknieto");
         minute = minute + x;
         if (minute >= 60)
         {
@@ -22,6 +56,15 @@ public class ClockTask: MonoBehaviour
         if (hour >= 24)
         {
             hour = 0;
+        }
+        if (minute < 0)
+        {
+            hour = hour - 1;
+            minute = 60 + minute;
+        }
+        if (hour < 0)
+        {
+            hour = 23;
         }
         string hourstr;
         if (hour < 10)
@@ -42,5 +85,52 @@ public class ClockTask: MonoBehaviour
             minstr = minute.ToString();
         }
         time.SetText(hourstr + ":" + minstr);
+    }
+
+    public void exitTask()
+    {
+        if (ktoryClock == 1)
+        {
+            if (hour == 0 && minute == 45)
+            {
+                win = 1;
+            }
+        }
+        else if (ktoryClock == 2)
+        {
+            if (hour == 7 && minute == 15)
+            {
+                win = 1;
+            }
+        }
+        else if (ktoryClock == 3)
+        {
+            if (hour == 9 && minute == 0)
+            {
+                win = 1;
+            }
+        }
+        else if (ktoryClock == 4)
+        {
+            if (hour == 4 && minute == 5)
+            {
+                win = 1;
+            }
+        }
+        else if (ktoryClock == 5)
+        {
+            if (hour == 4 && minute == 45)
+            {
+                win = 1;
+            }
+        }
+        else if (ktoryClock == 6)
+        {
+            if (hour == 10 && minute == 55)
+            {
+                win = 1;
+            }
+        }
+        SceneManager.LoadScene("SampleScene");
     }
 }
